55efcd085728a7d65edbff77c8ec4cb5f2cbecd4
dushmshr <dushmshr@gmail.com>
2018-02-23 10:56:41 +0530

    euro to rupee

--- EpPos2/settings.py
+++ EpPos2/settings.py
@@ -140,9 +140,7 @@ try:
 except ImportError as e:
     pass  # Doing nothing is OK.
 
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "live-static-files", "media-root")
