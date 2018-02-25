e95bdb2ba6c472e920915900f0fae054840b1aa4
dushmshr <dushmshr@gmail.com>
2018-02-26 00:03:30 +0530

    admin

--- pos/models.py
+++ pos/models.py
@@ -68,8 +68,8 @@ class Order_Item(models.Model):
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name','price','stock')
 
-class CashAdmin(models.ModelAdmin):
+class CashAdmin(admin.ModelAdmin):
     list_display = ('amount')
 
-class Order_ItemAdmin(models.ModelAdmin):
+class Order_ItemAdmin(admin.ModelAdmin):
     list_display = ('product','price','timestamp')
