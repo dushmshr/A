b8957c965370c0284ddf727a2938452acf7876e3
dushmshr <dushmshr@gmail.com>
2018-02-26 02:44:04 +0530

    order_item

--- pos/admin.py
+++ pos/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Product, Cash, Order,ProductAdmin,Order_Item
+from .models import Product, Cash, Order,ProductAdmin,Order_Item,Order_ItemAdmin
 
 # Register your models here.
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Cash)
 admin.site.register(Order)
-admin.site.register(Order_Item)
+admin.site.register(Order_Item,Order_ItemAdmin)
 
